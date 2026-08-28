9b1292f6ac575908bbe179129b20b91fada81dcd
multibiitt <multibit@atomicmail.io>
2026-08-28 11:41:49 +0300

    Fix multi-page build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  server: {
-    port: 5173,
-  },
+build: {
+rollupOptions: {
+input: {
+main: resolve(__dirname, 'index.html'),
+about: resolve(__dirname, 'about.html'),
+contact: resolve(__dirname, 'contact.html'),
+products: resolve(__dirname, 'products.html'),
+productDetail: resolve(__dirname, 'product-detail.html'),
+dashboard: resolve(__dirname, 'dashboard.html'),
+},
+},
+},
+server: {
+port: 5173,
+},
 });
